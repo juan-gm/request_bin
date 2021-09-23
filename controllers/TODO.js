4df6ea8520884a5c63d46230d362079c9c2f1ba2
José Agustín de la Puente <14jdelap@gmail.com>
2021-09-22 19:10:29 -0500

    Merge routing code

--- controllers/TODO.js
+++ controllers/TODO.js
@@ -1,18 +1,18 @@
-// TODO
-// In this file we will write the route handlers,
-// also called controllers
-// We could also write them all in app.js
-// because that's where they will be used
-// but here they can be better structured
-
 const exampleRouter = require('express').Router()
 
+router.post("/bins", (req, res) => {
+  // Creates a new random bin id in the database
+  // redirects the user to their new bin
+  url = generateRandomAlphanumericLength30()
+  res.redirect(`/bins/${url}`)
+})
 
 exampleRouter.get('/', (req, res) => {
+  // Renders the index page
   res.render('main', {layout : 'index'})
 })
 
-exampleRouter.get('/r/:url', (req, res) => {
+exampleRouter.get('/bins/:url', (req, res) => {
   // TODO
   // Check if the URL exists in the database
   // For now we will suppose that it exists
@@ -25,23 +25,20 @@ exampleRouter.get('/r/:url', (req, res) => {
   const inspection = req.query.inspect
 
   if (inspection == null) {
-    // TODO
-    // This counts as a request then. It should be added to the database
-    console.log('add this request')
-    res.render('newRequest', {layout : ''})
+    // This GET request should be collected because it doesn't inspect
+    // After collection, redirect to inpect page
+    // Assumes the user will want to inspect their most recent request to the bin
+
+    // res.render('newRequest', {layout : ''})
+    res.send(`This is ${req.query.test}`)
   }
 
-  /*
-    Check if any request has been made for this particular url
-    If no request has been made yet, load the page for
-    when it is empty with al the examples
-
-    If a request has been made, load the page which
-    shows all the requests
-  */
   if (requestsExist(req.url)) {
+    // Load page which shows existing requests
     res.render('existing', {layout : 'index', binURL: getBin(req.url)})
   } else {
+    // Since they have no requests
+    // Load page to show user how to make requests
     res.render('empty', {layout : 'index', binURL: getBin(req.url)})
   }
 })
@@ -52,6 +49,13 @@ requestsExist = (url) => {
   return false
 }
 
-getBin = (url) => 'https://requestbin.net' + url
+// Should extract this function to a file?
+function generateRandomAlphanumericLength30() {
+  // Generates an alphanumeric string of length 30
+  // Makes duplicate strings extremely unlikely
+  return Array.from(Array(20), () => Math.floor(Math.random() * 36).toString(36)).join('');
+}
+
+getBin = (path) => 'https://requestbin.net' + path
 
 module.exports = exampleRouter
